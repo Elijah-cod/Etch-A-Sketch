6799a64dcb86f47fc1bd4c2306ad966dfcd8ae94
Elijah <iamngunjiri@gmail.com>
2023-03-16 21:45:39 +0300

    Added a new button for generating mixed colors, and I created a loop to ensure that once you leave the container the color will stop generating when you return to the container without clicking any of the color generating buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('#container');
-// Created the squares by rows
+// Created the squares by rows and columns
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement('div');
@@ -10,23 +10,30 @@ for (let i = 0; i < 16; i++) {
         container.appendChild(square);
     }
 }
+// Created a counteractive mechanism to ensure that once the mouse leaves the container the previous colors stop regenerating
+let active = true;
 
+container.addEventListener("mouseleave", ()=>active=false);
+
+//creates a black background color once you hover over the pixels
 const black = document.getElementById("black");
 black.addEventListener("click", ()=>{
     //get the grids
+    active = true;
     const squares = document.querySelectorAll(".square");
     squares.forEach(function(square) {
-        square.addEventListener('mouseenter', function() {
-          this.style.backgroundColor = 'black';
+        square.addEventListener('mouseenter', function(){
+            if (active) {
+                this.style.backgroundColor = 'black';
+            }
         })
+            
     })
 
 });
 
-// const squares = document.querySelectorAll(".square");
-// squares.addEventListener('click', ()=>{
-//     this.style.backgroundColor = '';
-// })
+
+//erases the background color of the pixels once you hover over them
 
 const erase =document.getElementById("erase");
 erase.addEventListener("click", ()=>{
@@ -38,19 +45,33 @@ erase.addEventListener("click", ()=>{
     })
 });
 
+
+//clears all the background colors
+
 const clear = document.getElementById("clear");
 clear.addEventListener("click", ()=>{
+    active = false
     const squares = document.querySelectorAll(".square");
     squares.forEach(function(square) {
         square.style.backgroundColor ="";
     })
 });
 
-// const compButton = document.getElementById("computerSelection");
-// compButton.addEventListener("click", ()=>{
-//     computerSelection = list [Math.floor(Math.random()*3)];
-//     const results = document.querySelector(".results");
-//     if (computerSelection !==""){
-//         results.innerHTML=`Computer selection: ${computerSelection}`;//Add a div for displaying results
-//     }
-// });
+// in the code below we generated random numbers for when the mouse hovers over the pixels
+
+const rainbow = document.getElementById("rainbow");
+rainbow.addEventListener("click", ()=>{
+    active =true;
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 251);
+    }
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(function(square) {
+        square.addEventListener('mouseenter', function() {
+            if (active){
+                this.style.backgroundColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
+            }
+        })
+    })
+    
+});
